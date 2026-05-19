4f54dfa08ef5cd34f2792851df7b1bb148c9c903
Mikhail Burdukovsky <burmspb@gmail.com>
2026-05-19 12:17:15 +0000

    creating templates and controllers

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from catalog.apps import NewappConfig
+from catalog.views import home_view, contacts_view
 
-app_name = NewappConfig.name
+app_name = 'catalog'
 
 urlpatterns = [
-    path('',)
+    # Главная страница (Каталог)
+    path('', home_view, name='home'),
+    
+    # Страница контактов
+    path('contacts/', contacts_view, name='contacts'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# Логика для главной страницы
+def home_view(request):
+    return render(request, 'catalog/home.html')
+
+# Логика для контактов
+def contacts_view(request):
+    return render(request, 'catalog/contacts.html')
