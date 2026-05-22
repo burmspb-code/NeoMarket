c6261fb4270af76b893433d364c65e458f4dfa33
Mikhail Burdukovsky <burmspb@gmail.com>
2026-05-22 20:21:05 +0000

    add library app

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 from catalog.views import home_view, contacts_view
 
-app_name = 'catalog'
+app_name = "catalog"
 
 urlpatterns = [
     # Главная страница (Каталог)
-    path('', home_view, name='home'),
-    
+    path("", home_view, name="home"),
     # Страница контактов
-    path('contacts/', contacts_view, name='contacts'),
+    path("contacts/", contacts_view, name="contacts"),
 ]
