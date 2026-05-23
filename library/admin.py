f2724456fef78c2c36c193c877d5f36301873894
Mikhail Burdukovsky <burmspb@gmail.com>
2026-05-23 07:06:35 +0000

    add admin panel

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Author, Book
 
-# Register your models here.
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    # Колонки в общем списке
+    list_display = ('first_name', 'last_name', 'birth_date')
+
+    # Выбираем фильтр
+    list_filter = ('last_name',)
+
+    # Выбираем поля для поиска
+    search_fields = ('first_name', 'last_name')
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    # Колонки в общем списке
+    list_display = ('title', 'publication_date', 'author')
+
+    # Выбираем фильтр
+    list_filter = ('author',)
+
+    # Выбираем поля для поиска
+    search_fields = ('title',)
+
