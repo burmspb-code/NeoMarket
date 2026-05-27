cd498658221a8acc2b0849593e907929ca1bfd6e
Mikhail Burdukovsky <burmspb@gmail.com>
2026-05-27 13:13:36 +0000

    add command custom

--- catalog/admin.py
+++ catalog/admin.py
@@ -4,11 +4,20 @@ from catalog.models import Category, Product
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
-    
+    list_display = ("id", "name")
+
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'description','image', 'category', 'price', 'created_at', 'updated_at')
-    list_filter = ('category',)
-    search_fields = ('name', 'description')
+    list_display = (
+        "id",
+        "name",
+        "description",
+        "image",
+        "category",
+        "price",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = ("category",)
+    search_fields = ("name", "description")
