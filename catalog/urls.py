a84a8761f8fed5b4472ce74588f50301b33e4096
Mikhail Burdukovsky <burmspb@gmail.com>
2026-06-02 09:31:29 +0000

    added navigation menu

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home_view, contacts_view, product_detail
+from catalog.views import home_view, contacts_view, catalog_view, product_detail
 
 app_name = CatalogConfig.name
 
@@ -9,5 +9,8 @@ urlpatterns = [
     path("", home_view, name="home"),
     # Страница контактов
     path("contacts/", contacts_view, name="contacts"),
+    # Страница с каталогом товаров
+    path("catalog/", catalog_view, name="catalog_list"),
+    # Страница с детальной информацией о товаре
     path("product/<int:pk>/", product_detail, name="product_detail"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -5,9 +5,20 @@ from catalog.models import Product
 
 # Логика для главной страницы
 def home_view(request):
-    products = Product.objects.all()
-    return render(request, "catalog/home.html", {'products':products})
+    products = Product.objects.all()[:3] # Берем первые три товара
+    context = {'products':products}
+    return render(request, 'catalog/index.html', context)
 
+# Логика для страницы каталога
+def catalog_view(request):
+    products = Product.objects.all()
+    context = {'products':products}
+    return render(request, "catalog/catalog_view.html", context)
+
+# Логика для страницы детального описания товара
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, "catalog/product_detail.html", {'product':product})
 
 # Логика для контактов с формой обратной связи
 def contacts_view(request):
@@ -22,10 +33,4 @@ def contacts_view(request):
             request,
             "Ваше сообщение успешно отправлено! Мы свяжемся с вами в ближайшее время.",
         )
-
     return render(request, "catalog/contacts.html")
-
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    return render(request, "catalog/product_detail.html", {'product':product})
-
