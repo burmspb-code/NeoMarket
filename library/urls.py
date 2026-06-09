63375bc590429bdc109e6e8ea6cd4b6338cdae75
Mikhail Burdukovsky <burmspb@gmail.com>
2026-06-09 09:44:06 +0000

    creating views with CBV

--- library/urls.py
+++ library/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from library.apps import LibraryConfig
-from library.views import books_list, book_detail
+# from library.views import books_list, book_detail
 
 app_name = LibraryConfig.name
 
 urlpatterns = [
-    path("library_list/", books_list, name="books_list"),
-    path("library_detail/<int:book_id>/", book_detail, name="book_detail"),
+    # path("library_list/", books_list, name="books_list"),
+    # path("library_detail/<int:book_id>/", book_detail, name="book_detail"),
 ]
