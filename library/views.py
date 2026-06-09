d55d0024a1842dc807f2f683c34df99d329ce00f
Mikhail Burdukovsky <burmspb@gmail.com>
2026-06-09 18:52:36 +0000

    add librarys templates

--- library/views.py
+++ library/views.py
@@ -8,32 +8,31 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 class BooksListView(ListView):
     model = Book
-    template_name = 'books_list.html'
+    template_name = 'library/books_list.html'
     context_object_name = 'books'
 
 
 class BookCreateView(CreateView):
     model = Book
     fields = '__all__'
-    template_name = 'book_form.html'
+    template_name = 'library/book_form.html'
     success_url = reverse_lazy('library:books_list')
 
 
 class BookDetailView(DetailView):
     model = Book
-    template_name = 'book_detail.html'
+    template_name = 'library/book_detail.html'
     context_object_name = 'book'
 
 
-class BookUpdateViews(UpdateView):
+class BookUpdateView(UpdateView):
     model = Book
     fields = '__all__'
-    template_name = 'book_form.html'
+    template_name = 'library/book_form.html'
     success_url = reverse_lazy('library:books_list')
 
 
 class BookDeleteView(DeleteView):
     model = Book
-    template_name = 'book_confirm_delete.html'
+    template_name = 'library/book_confirm_delete.html'
     success_url = reverse_lazy('library:books_list')
-
