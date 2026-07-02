d83955d7e1f7e1fc6a42c2d291db441c5026a48d
Mikhail Burdukovsky <burmspb@gmail.com>
2026-07-02 18:41:47 +0300

    Feature/library auth (#10)
    * create auth users
    
    * added user authorization

--- config/urls.py
+++ config/urls.py
@@ -2,12 +2,21 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    # Главная страница: выводит пустой шаблон «из коробки»
+    path("", TemplateView.as_view(template_name="catalog/empty.html"), name="home"),
+    # Страница для администрирования
     path("admin/", admin.site.urls),
-    path("", include("catalog.urls", namespace="catalog")),
+    # Страница для администрирования
+    path("catalog/", include("catalog.urls", namespace="catalog")),
+    # Страница интернет магазина
     path("blog/", include("blog.urls", namespace="blog")),
+    # Страница библиотеки книг
     path("library/", include("library.urls", namespace="library")),
+    # Страница регистрации пользователей
+    path("users/", include("users.urls", namespace="users")),
 ]
 
 # Настройка для раздачи медиафайлов в режиме разработки
