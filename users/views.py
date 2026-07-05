a6588cc0a873ac6851385ee4933193487277cfae
Mikhail Burdukovsky <burmspb@gmail.com>
2026-07-05 20:16:00 +0300

    added email sending during user registration

--- users/views.py
+++ users/views.py
@@ -9,45 +9,27 @@ from .forms import CustomUserCreationForm
 
 
 class RegisterView(CreateView):
-    """Представление для регистрации нового пользователя.
-
-    Использует кастомную форму регистрации и перенаправляет пользователя
-    на список книг после успешного создания аккаунта.
-    Attributes:
-        form_class: Класс формы для валидации и создания пользователя.
-        template_name (str): Путь к HTML-шаблону страницы для регистрации.
-        success_url (str): Путь для перенаправления после успешной регистрации.
-    """
+    """Представление для регистрации нового пользователя."""
 
     form_class = CustomUserCreationForm
     template_name = "users/register.html"
     success_url = reverse_lazy("catalog:catalog_list")
 
     def form_valid(self, form):
-        """Вызывается при успешной валидации данных.
-        Сохраняет пользователя в базу данных, выполняет автоматический вход в систему и
-        перенаправляет на целевую страницу.
-        """
-        # Сначала сохраняем пользователя в БД через родительский метод
+        """Вызывается при успешной валидации данных."""
+        # super().form_valid сохраняет юзера в базу и создает объект HttpResponseRedirect
         response = super().form_valid(form)
 
-        # self.object содержит созданного пользователя — авторизуем его
+        # Логиним пользователя (self.object уже создан родительским методом)
         login(self.request, self.object)
 
-        # Отправляем письмо на почту
-        self.send_welcome_email(self.object.email)
+        # Отправляем приветственное письмо
+        send_mail(
+            'Добро пожаловать на NeoMarket!',
+            'Спасибо, что зарегистрировались на нашем сайте!',
+            settings.DEFAULT_FROM_EMAIL,
+            [self.object.email],
+            fail_silently=True
+        )
 
         return response
-
-    def send_welcome_email(self, user_email):
-        """Отправка приветственного письма на почту.
-            Поля:
-            subject (str): Заголовок письма.
-            messages (str): Сообщение письма.
-            recipient_list (list): Список потовых адресов для отправки.
-        """
-        subject = 'Добро пожаловать на наш сервис!'
-        message = 'Спасибо, что зарегистрировались на нашем сервисе!'
-        from_email = settings.DEFAULT_FROM_EMAIL
-        recipient_list = [user_email,]
-        send_mail(subject, message, from_email, recipient_list)
